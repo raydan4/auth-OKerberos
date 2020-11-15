9dcd5231c963e35d47d5d94e81d48e9a833ee763
NotoriousRebel <matt12299@yahoo.com>
2020-11-15 14:52:02 -0500

    Initial commit of auth server.

---
+++ auth/server.py
@@ -0,0 +1,66 @@
+import aiohttp
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+from base64 import b64encode
+import ujson
+from Crypto.Hash import SHA256
+
+import crypto
+from config import key
+
+app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class User(BaseModel):
+    username: str
+    password: str
+
+
+async def make_request(url: str, username: str, password: str) -> dict:
+    timeout = aiohttp.ClientTimeout(total=35)
+    params = {"username": username, "password": password}
+    async with aiohttp.ClientSession(timeout=timeout) as session:
+        async with session.post(url, params=params) as resp:
+            text = await resp.text("UTF-8")
+            resp = ujson.loads(text)
+            return resp
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    try:
+        url = "http://validate.php"
+        resp = await make_request(url, form_data.username, form_data.password)
+        valid, token = resp["valid"], resp["token"]
+        h = SHA256.new()
+        h.update(form_data.password.encode("UTF-8"))
+        hash = h.hexdigest()
+        resp = str(crypto.aes256_encrypt(key, bytes(token)))
+        return b64encode(
+            crypto.aes256_encrypt(hash, f'{{"auth": "fail", "token": {resp if valid else ""}}}'.encode("UTF-8")))
+    except Exception as e:
+        print(f'An exception has occurred: {e}')
+        return [{"error": e}]
+
+
+def test():
+    # token =
+    # resp = str(crypto.aes256_encrypt(key, bytes(token)))
+    h = SHA256.new()
+    h.update("password".encode("UTF-8"))
+    hash = h.digest()
+    # resp = str(crypto.aes256_encrypt(key, bytes(token)))
+    temp = b64encode(crypto.aes256_encrypt('{"auth": "fail", "token": ""}'.encode('UTF-8'), hash))
+    print(f'encoded: {temp}')
+
+
+if __name__ == '__main__':
+    test()
